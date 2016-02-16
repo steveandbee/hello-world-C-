6eba2c7b18071b89c89a435c9000965d9edd1feb
steveandbee <steveandbee65@gmail.com>
2016-02-16 13:24:24 +0000

    Update main.cpp
    "//" added in line 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-This will make build fail and trigger Jenkins as a result of SCM push - so will see failure
+// This will make build fail and trigger Jenkins as a result of SCM push - so will see failure
 int main(){
   std::cout << "Hello world, hello Jenkins";
   }

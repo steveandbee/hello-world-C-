aae94efca2827c40acce4278fda11706b51d8d88
steveandbee <steveandbee65@gmail.com>
2016-02-19 17:50:54 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,5 @@
 // This will make build fail and trigger Jenkins as a result of SCM push - so will see failure
 // Another line to trigger SCM build on Jenkins - testing
 int main(){
-  std::cout << "Hello world, hello Jenkins;
+  std::cout << "Hello world, hello Jenkins";
   }
